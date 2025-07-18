cd478214e1dc85f25706a0956bd823e527224eee
Shihab Raafat <52350730+ShihabRaafatTechnology@users.noreply.github.com>
2025-07-18 22:03:46 +0300

    Add files via upload

--- eCommerce-Front/src/components/form/input/index.tsx
+++ eCommerce-Front/src/components/form/input/index.tsx
@@ -12,8 +12,8 @@ type TInputProps<TFieldValue extends FieldValues> = {
 const Input = <TFieldValue extends FieldValues>({ label, name, type = "text", register, error }: TInputProps<TFieldValue>) => {
     return (
         <div className="flex flex-col my-4">
-            <label className="text-gray-700">{label}</label>
-            <input type={type} className="mt-2 p-2 border border-gray-300 rounded text-sm text-gray-900" {...register(name)} />
+            <label className="text-gray-700 text-lg">{label}</label>
+            <input type={type} className="mt-2 p-3 border border-gray-300 rounded text-lg text-gray-900" {...register(name)} />
             <span className="text-red-400 italic">{error}</span>
         </div>
     )
--- eCommerce-Front/src/pages/Register.tsx
+++ eCommerce-Front/src/pages/Register.tsx
@@ -15,14 +15,14 @@ const Register = () => {
   return (
     <div className="w-full min-h-screen flex items-center justify-center absolute top-0 z-50">
       <div className="w-full h-screen flex items-center justify-center">
-        <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 h-full bg-white flex items-center justify-center">
+        <div className="w-full sm:w-5/6 md:w-2/3 lg:w-1/2 h-full bg-white flex items-center justify-center">
           <div className="w-full px-12">
-            <h2 className="text-center text-2xl font-bold tracking-wide text-gray-800">Register</h2>
-            <p className="text-center text-sm text-gray-600 mt-2">
+            <h2 className="text-center text-4xl font-bold tracking-wide text-gray-800">Register</h2>
+            <p className="text-center text-lg text-gray-600 mt-2">
               Already have an account? <Link to="/sign-in" className="text-blue-600 hover:text-blue-700 hover:underline">Sign in here</Link>
             </p>
 
-            <form className="my-8 text-sm" onSubmit={handleSubmit(submitForm)}>
+            <form className="my-8 text-lg" onSubmit={handleSubmit(submitForm)}>
               <Input
                 label="First Name"
                 name="firstName"
@@ -59,8 +59,8 @@ const Register = () => {
 
 
               <div className="flex items-center">
-                <input type="checkbox" id="remember_me" name="remember_me" className="mr-2 rounded" />
-                <label htmlFor="remember_me" className="text-gray-700">
+                <input type="checkbox" id="remember_me" name="remember_me" className="mr-2 rounded p-2" />
+                <label htmlFor="remember_me" className="text-gray-700 text-xl">
                   I accept the <a href="#" className="text-blue-600 hover:text-blue-700 hover:underline">terms</a> and <a href="#" className="text-blue-600 hover:text-blue-700 hover:underline">privacy policy</a>
                 </label>
               </div>
@@ -72,32 +72,22 @@ const Register = () => {
 
             <div className="flex items-center justify-between">
               <div className="w-full h-[1px] bg-gray-300"></div>
-              <span className="text-sm uppercase mx-6 text-gray-400">Or</span>
+              <span className="text-lg uppercase mx-6 text-gray-400">Or</span>
               <div className="w-full h-[1px] bg-gray-300"></div>
             </div>
 
-            <div className="text-sm">
-              <a href="#" className="flex items-center justify-center space-x-2 text-gray-600 my-2 py-2 bg-gray-100 hover:bg-gray-200 rounded">
+            <div className="text-lg">
+              <a href="#" className="flex items-center justify-center space-x-2 text-gray-600 my-2 py-4 bg-gray-100 hover:bg-gray-200 rounded">
                 <span>Sign up with Google</span>
               </a>
-              <a href="#" className="flex items-center justify-center space-x-2 text-gray-600 my-2 py-2 bg-gray-100 hover:bg-gray-200 rounded">
+              <a href="#" className="flex items-center justify-center space-x-2 text-gray-600 my-2 py-4 bg-gray-100 hover:bg-gray-200 rounded">
                 <span>Sign up with Facebook</span>
               </a>
             </div>
 
           </div>
         </div>
-        <div className="hidden lg:flex lg:w-1/2 xl:w-2/3 2xl:w-3/4 h-full bg-cover" style={{ backgroundImage: "url('https://vojislavd.com/ta-template-demo/assets/img/auth-background.jpg')" }}>
-          <div className="w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-30">
-            <div className="flex items-center justify-center space-x-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16 xl:h-20 xl:w-20 2xl:h-24 2xl:w-24 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5"></path>
-              </svg>
-              <h1 className="text-3xl xl:text-4xl 2xl:text-5xl font-bold text-gray-100 tracking-wider">Template</h1>
-            </div>
-            <p className="text-gray-300 mt-4 px-16 text-center">Free admin dashboard template created with Tailwind CSS and Alpine.js</p>
-            <a href="#" className="mt-6 bg-gray-100 hover:bg-gray-200 px-6 py-2 rounded text-sm uppercase text-gray-900 transition duration-150" title="Learn More">Learn More</a>
-          </div>
+        <div className="hidden lg:flex lg:w-1/2 h-full bg-cover" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1562408590-e32931084e23?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
         </div>
       </div>
     </div>
